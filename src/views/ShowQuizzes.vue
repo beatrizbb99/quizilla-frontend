62e7d0898cd8edb9506a77c7ed5e524c2e835284
Sadia Miah <sadia.miah@student.hs-rm.de>
2024-06-26 23:52:52 +0200

    Suchleiste

--- src/views/ShowQuizzes.vue
+++ src/views/ShowQuizzes.vue
@@ -4,33 +4,40 @@
       <button @click="selectedOption = 'all'">Alle Quizze</button>
       <button @click="selectedOption = 'my'">Meine Quizze</button>
     </div>
+    <div class="search-bar">
+      <input v-model="searchQuery" placeholder="Suche nach Quizze..." />
+    </div>
     <ul v-if="selectedOption === 'all'">
-      <li v-for="quiz in quizzes" :key="quiz.id">
+      <li v-for="quiz in filteredQuizzes" :key="quiz.id">
         <QuizCard :quizData="quiz" />
       </li>
     </ul>
     <ul v-else-if="selectedOption === 'my'">
-      <MyQuizzes :myQuizData="quizzes" />
+      <MyQuizzes :myQuizData="filteredQuizzes" />
     </ul>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
 import { quizzes, getAllQuizzes } from '@/services/quiz.database.handler.js';
 import QuizCard from '@/components/QuizCard';
 import MyQuizzes from '@/components/MyQuizzes';
 
 const selectedOption = ref('all');
+const searchQuery = ref('');
 
 const fetchQuizzes = async () => {
   await getAllQuizzes();
 };
 
+const filteredQuizzes = computed(() => {
+  return quizzes.value.filter(quiz => 
+    quiz.name.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
+});
 
 onMounted(() => {
   fetchQuizzes();
 });
 </script>
-
-<style scoped></style>
